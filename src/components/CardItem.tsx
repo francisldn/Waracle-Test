065c9d3ece35e34e396fde809dbe1037cb727491
Francis_ldn <fnhh04@gmail.com>
2022-11-09 12:00:52 +0000

    remove cursor-pointer

--- src/components/CardItem.tsx
+++ src/components/CardItem.tsx
@@ -11,7 +11,7 @@ export default function CardItem({cat}:Props) {
   const dispatch = useAppDispatch()
   const {id, imageURL, breedName, temperament, origin, lifeSpan, wikipediaUrl, vote} = cat
   return (
-    <div className="flex flex-col rounded-xl w-[90%] mx-auto overflow-hidden border-gray-300 border-2 cursor-pointer shadow-gray-800 mb-4 box-effect">
+    <div className="flex flex-col rounded-xl w-[90%] mx-auto overflow-hidden border-gray-300 border-2 shadow-gray-800 mb-4 box-effect">
         <div className="overflow-hidden h-[300px] img-area">
             <span></span>
             <img src={imageURL} alt="" className="w-full h-full object-cover"/>

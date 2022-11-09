fef2d8d1f9cd606ea9f99597277426ef110d5ff9
Francis_ldn <fnhh04@gmail.com>
2022-11-09 11:56:17 +0000

    edit App

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import React from "react"
 import "./App.css"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
-import SingleCat from "./pages/SingleCat"
+// import SingleCat from "./pages/SingleCat"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path=':id' element={<SingleCat />} />
+        {/* <Route path=':id' element={<SingleCat />} /> */}
       </Routes>
     </BrowserRouter>
   )

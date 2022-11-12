600817c99954cb97464208d1895b8ca0ccd1d8cb
Francis_ldn <fnhh04@gmail.com>
2022-11-12 13:25:08 +0000

    format code

--- src/components/ErrorFallBack.tsx
+++ src/components/ErrorFallBack.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
-
+import React from "react"
 
 export default function ErrorFallBack() {
   return (
     <div>
-        <img src="error.gif" alt="error fallback" />
+      <img src='error.gif' alt='error fallback' />
     </div>
   )
 }
